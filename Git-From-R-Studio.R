37f6313894f25f8f5cad1b36f4e9bb1d1912fa82
Jesse Gordon <jesse.gordon@ranchobiosciences.com>
2021-07-23 09:44:28 -0400

    R-Studio Sequence is "Stage > Commit message > Commit > Push "

---
+++ Git-From-R-Studio.R
@@ -0,0 +1,8 @@
+# Jesse's GitHub-linked script
+
+print ("This file was created by Jesse in R-Studio")
+
+print ("Spelling error corrected in R and 'pulled'; this line added in Notepad - does it commit?")
+
+print ("So process in R-Studio is just 'Save'? Then Commit from 'Git' tab")
+
