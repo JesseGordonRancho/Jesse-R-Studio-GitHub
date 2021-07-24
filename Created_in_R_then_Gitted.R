6e0bf90efe0380506536e01365c9d00592ba02a3
Jesse Gordon <jesse.gordon@ranchobiosciences.com>
2021-07-24 09:23:48 -0400

    So far, staged then commit

---
+++ Created_in_R_then_Gitted.R
@@ -0,0 +1,8 @@
+# GitHub-to-RStudio is the problem -- let's resolve it
+print("This file was created within RStudio")
+print("Goal is to get it onto GitHub, but I can't believe htis is as difficult as it seems")
+# They say: "All files that have been modified since your last pull appear in the Git tab"
+# When you click "Staged" it is not instantaneous -- there's a few second-delay, like something is wrong -- so click and wait!
+# Next... Click “Commit”. A new window should open, that lists all of the changed files from earlier, and below that shows the differences in the staged files from previous versions. In the upper quadrant, in the “Commit message” box, write yourself a commit message. Click Commit. Close the window.
+
+
