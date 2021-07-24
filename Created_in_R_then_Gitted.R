0de1975721c6c7e20bf06a11ef4a6459460f0258
Jesse Gordon <jesse.gordon@ranchobiosciences.com>
2021-07-24 09:26:49 -0400

    save > stage > comment > commit > push

--- Created_in_R_then_Gitted.R
+++ Created_in_R_then_Gitted.R
@@ -4,5 +4,7 @@ print("Goal is to get it onto GitHub, but I can't believe htis is as difficult a
 # They say: "All files that have been modified since your last pull appear in the Git tab"
 # When you click "Staged" it is not instantaneous -- there's a few second-delay, like something is wrong -- so click and wait!
 # Next... Click “Commit”. A new window should open, that lists all of the changed files from earlier, and below that shows the differences in the staged files from previous versions. In the upper quadrant, in the “Commit message” box, write yourself a commit message. Click Commit. Close the window.
-
+# Next... So far, you have created a file, saved it, staged it, and committed it. If you remember your version control lecture, the next step is to push your changes to your online repository. Push your changes to the GitHub repository.
+# Ok, so it's save > stage > comment > commit > push. Who invented that?!
+print("And it only shows up in Git tab if there's a change since the last save!")
 
